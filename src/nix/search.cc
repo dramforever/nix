39a409cfd113afa71e88cdc1d8f745f49ed36598
dramforever <dramforever@live.com>
2022-01-31 21:28:31 +0800

    Generate metadata for nix search

--- src/nix/search.cc
+++ src/nix/search.cc
@@ -11,6 +11,8 @@
 #include "nix/expr/attr-path.hh"
 #include "nix/util/hilite.hh"
 #include "nix/util/strings-inline.hh"
+#include "nix/util/fmt.hh"
+#include "nix/expr/value-to-json.hh"
 
 #include <regex>
 #include <fstream>
@@ -147,10 +149,13 @@ struct CmdSearch : InstallableValueCommand, MixJSON
                     if (found) {
                         results++;
                         if (json) {
+                            NixStringContext context;
+
                             (*jsonOut)[attrPathStr] = {
                                 {"pname", name.name},
                                 {"version", name.version},
                                 {"description", description},
+                                {"meta", printValueAsJSON(*state, true, aMeta->forceValue(), noPos, context, false)}
                             };
                         } else {
                             if (results > 1)
